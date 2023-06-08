45b84663d75ae31934c4b6bee5768a3692c2c045
Vincenzo Tramo <vv.tramo@gmail.com>
2023-06-08 20:59:29 +0200

    feat: add actfun module
    This commit adds the DerivableFunction abstract base class and the SigmoidActivationFunction class. The DerivableFunction class provides a foundation for derivable functions with the __call__ and derivative methods. The SigmoidActivationFunction class is a specific implementation of the sigmoid activation function, providing the sigmoid function value and its derivative. These additions contribute to the functionality of the module for working with derivable functions in neural networks.

---
+++ nnkit/actfun.py
@@ -0,0 +1,48 @@
+import numpy
+
+
+class DerivableFunction:
+    """
+    An abstract base class representing a derivable function.
+
+    Methods:
+        __call__(x: numpy.array) -> numpy.array:
+            Compute the function value for the given input.
+
+        derivative(x: numpy.array) -> numpy.array:
+            Compute the derivative of the function for the given input.
+
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, x: numpy.array) -> numpy.array:
+        raise NotImplementedError
+
+    def derivative(self, x: numpy.array) -> numpy.array:
+        raise NotImplementedError
+
+
+class SigmoidActivationFunction(DerivableFunction):
+    """
+    A class representing the Sigmoid activation function.
+
+    Methods:
+        __call__(x: numpy.array) -> numpy.array:
+            Compute the Sigmoid function value for the given input.
+
+        derivative(x: numpy.array) -> numpy.array:
+            Compute the derivative of the Sigmoid function for the given input.
+
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, x: numpy.array) -> numpy.array:
+        return 1 / (1 + numpy.exp(-x))
+
+    def derivative(self, x: numpy.array) -> numpy.array:
+        sigma_x = self(x)
+        return sigma_x * (1 - sigma_x)
