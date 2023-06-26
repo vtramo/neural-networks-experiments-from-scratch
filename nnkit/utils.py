3491a28d6cfe542e4db4848da9ac4a3ce3f29fef
Vincenzo Tramo <vv.tramo@gmail.com>
2023-06-26 09:51:44 +0200

    feat(utils): make fair_divide fun capable of accepting more than one iterable

--- nnkit/utils.py
+++ nnkit/utils.py
@@ -1,5 +1,6 @@
 from .neuronet import DenseNetwork
 from .lossfun import LossFunction
+from .neurotrain import DataLabelSet
 import numpy as np
 
 
@@ -8,31 +9,24 @@ def one_hot(labels, tot_classes=10):
     return np.array([(base == label) * 1 for label in labels])
 
 
-def fair_divide(iterable, workers: int) -> list[list, ...]:
-    partition = []
-    count = len(iterable) // workers
-    remainder = len(iterable) % workers
+def fair_divide(*iterables: list, workers: int) -> list[list]:
+    if len(iterables) == 0:
+        return []
 
-    for i in range(workers):
-        if i < remainder:
-            start = i * (count + 1)
-            stop = start + count
-        else:
-            start = i * count + remainder
-            stop = start + (count - 1)
-        partition.append(iterable[start:stop + 1])
+    def fair_partition(iterable) -> list[list]:
+        partition = []
+        count = len(iterable) // workers
+        remainder = len(iterable) % workers
 
-    return partition
+        for i in range(workers):
+            if i < remainder:
+                start = i * (count + 1)
+                stop = start + count
+            else:
+                start = i * count + remainder
+                stop = start + (count - 1)
+            partition.append(iterable[start:stop + 1])
 
+        return partition
 
-def calculate_accuracy_and_loss(net: DenseNetwork, points, labels, loss: LossFunction) -> tuple[float, float]:
-    correct_predictions = 0
-    total_loss = 0
-
-    for image, label in zip(points, labels):
-        net_output = net(image)
-
-        if np.argmax(net_output) == np.argmax(label):
-            correct_predictions += 1
-
-    return correct_predictions / len(points), total_loss / len(points)
+    return tuple(map(fair_partition, iterables))
