c67f7d0d08e23edef37860543b5613b210bd9cfd
RalphTheCreator <rafdimaso@gmail.com>
2023-07-05 16:56:24 +0200

    feat: add plot_training_histories

--- nnkit/plotlib.py
+++ nnkit/plotlib.py
@@ -38,3 +38,36 @@ def plot_training_history(
     if path:
         plt.savefig(path)
 
+
+def plot_training_histories(
+    train_histories: list[TrainingHistory],
+    metric: str,
+    show_plot: bool = True,
+    path: str = ''
+):
+    metric_values_by_name = {}
+    epochs = np.arange(1, train_histories[0].epochs + 1)
+    
+    for train_history in train_histories:
+        for _ in train_history.history:
+            for metric_name, metrics in _.items():
+                if metric_name == metric:
+                    new_metric_name = f'{train_history.update_rule}_{metric_name}'
+                    metric_values_by_name.setdefault(new_metric_name, []) 
+                    metric_values_by_name[new_metric_name].append(metrics.result())
+
+    for metric_name, metric_values in metric_values_by_name.items():
+            print(metric_name, metric_values)
+            plt.plot(epochs, metric_values, '-', label=metric_name, lw=1.2)
+
+    plt.xticks(epochs)
+    plt.xlabel('Epochs', color='black')
+    plt.ylabel(metric, color='black')
+    plt.title(f'{metric} across Training')
+    plt.legend()
+
+    if show_plot:
+        plt.show()
+
+    if path:
+        plt.savefig(path)
