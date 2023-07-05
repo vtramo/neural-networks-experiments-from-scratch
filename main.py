c758714caa0e6a11ada8cad195423ea09512e3a0
RalphTheCreator <rafdimaso@gmail.com>
2023-07-05 16:56:42 +0200

    test: add test loop for each update rule

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from matplotlib import pyplot as plt
+import numpy as np
 from nnkit.neuronet import DenseLayer, DenseNetwork
 from nnkit.activations import Softmax, ReLU
 from nnkit.losses import CrossEntropySoftmax
 from nnkit.datasets import mnist
 from nnkit.datasets.utils import DataLabelSet, one_hot
+from nnkit.plotlib import plot_training_histories, plot_training_history
 from nnkit.training.model_selection import KFold
-from nnkit.training.neurotrain import NetworkTrainer
+from nnkit.training.neurotrain import NetworkTrainer, TrainingHistory
 from nnkit.training.update_rules import SGD, RPropPlus, IRPropPlus, RPropMinus, IRPropMinus
 from nnkit.training.metrics import Accuracy
 
@@ -20,27 +23,30 @@ if __name__ == '__main__':
     # Load data / Data pre-processing
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
     train_images = train_images.reshape((60000, 28 * 28))
-    train_images = (train_images.astype('float32') / 255)[:237]
-    train_labels = one_hot(train_labels)[:237]
+    train_images = (train_images.astype('float32') / 255)[:100]
+    train_labels = one_hot(train_labels)[:100]
     test_images = test_images.reshape((10000, 28 * 28))
     test_images = test_images.astype('float32') / 255
     test_labels = one_hot(test_labels)
 
     # Evaluate the network
-    training_set = DataLabelSet(train_images, train_labels, batch_size=len(train_images), name='training')
+    training_set = DataLabelSet(train_images, train_labels, name='training')
+    training_set, validation_set = training_set.split(split_factor=0.3, split_set_name='validation')
 
-    trainer = NetworkTrainer(
-        net=net,
-        update_rule=IRPropPlus(),
-        loss_function=CrossEntropySoftmax(),
-        metrics=[Accuracy(name='accuracy')]
-    )
+    histories = []
 
-    kfold = KFold(k=5, shuffle=False)
-    results = []
-    for train_data, test_data in kfold(training_set):
-        history = trainer.train_network(train_data, test_data, epochs=1)
+    for update_rule in [RPropPlus(), IRPropPlus(), RPropMinus(), IRPropMinus()]:
+        
+        trainer = NetworkTrainer(
+            net=net,
+            update_rule=update_rule,
+            loss_function=CrossEntropySoftmax(),
+            metrics=[Accuracy(name='accuracy')]
+        )
+
         trainer.net.reset_parameters()
-        results.append(history.best_parameters.metric_results['test_accuracy'])
+        history = trainer.train_network(training_set, validation_set, epochs=5)
 
-    print([str(result) for result in results])
+        histories.append(history)
+    
+    plot_training_histories(histories, 'validation_accuracy', show_plot=True, path='validation_accuracy.png')
