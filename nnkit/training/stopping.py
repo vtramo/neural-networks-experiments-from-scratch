ed54282ac90af1f5a2b93321ef3fe51d1a07d4b0
Vincenzo Tramo <vv.tramo@gmail.com>
2023-07-11 17:51:25 +0200

    feat(stopping): add GLStoppingCriterion (generalization loss sc)

--- nnkit/training/stopping.py
+++ nnkit/training/stopping.py
@@ -14,3 +14,16 @@ class StoppingCriterion(metaclass=ABCMeta):
 
 def generalization_loss(val_loss: float, opt_val_loss: float) -> float:
     return 100 * ((val_loss / opt_val_loss) - 1)
+
+
+class GLStoppingCriterion(StoppingCriterion):
+
+    def __init__(self, alpha: float):
+        self.__alpha = alpha
+        self.__last_val_loss = None
+        self.__opt_val_loss = float('inf')
+
+    def __call__(self, training_data: TrainingData) -> bool:
+        self.__last_val_loss = training_data.val_loss
+        self.__opt_val_loss = min(self.__opt_val_loss, self.__last_val_loss)
+        return generalization_loss(self.__last_val_loss, self.__opt_val_loss) > self.__alpha
