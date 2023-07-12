243a6fd7a8ec76ad6672b12c30f40843d0ec5a29
Raffaele Di Maso <rafdimaso@gmail.com>
2023-07-12 16:44:47 +0200

    refactor(lossess): removed loss fix

--- nnkit/core/losses.py
+++ nnkit/core/losses.py
@@ -23,7 +23,7 @@ class CrossEntropy(LossFunction):
 
     def __call__(self, predictions: np.ndarray, gold_labels: np.ndarray) -> float:
         axis = int((predictions.ndim == 2))
-        return -np.sum(gold_labels * np.log(predictions + 1e-10), axis)
+        return -np.sum(gold_labels * np.log(predictions), axis)
 
     def output_derivative(self, prediction: np.ndarray, gold_label: np.ndarray) -> np.ndarray:
         return (prediction - gold_label) / (prediction * (1 - prediction))
